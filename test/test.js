7e3780a3912aa06a6a8db3b33e494ecff7a715ea
wenan <login@wenanle.com>
2017-09-12 17:15:06 +0800

    oh easy

--- test/test.js
+++ test/test.js
@@ -78,7 +78,7 @@
   };
 
   logger = function(m) {
-    return console.log("mongoose-sex-page:" + m);
+    return console.log("[mongoose-sex-page]: " + m);
   };
 
   mongoose.connect('mongodb://localhost/pagnation', {
